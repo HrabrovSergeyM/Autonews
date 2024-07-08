e2ed939e291419d819375faf5173bff4e43f301d
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2024-07-08 23:28:02 +0300

    Initial Commit

---
+++ Autonews/AutonewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutonewsApp.swift
+//  Autonews
+//
+//  Created by Sergey Hrabrov on 08.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AutonewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
